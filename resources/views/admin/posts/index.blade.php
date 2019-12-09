73b1a8fb95358b670e8b17598eca8af8ce5ddd75
portofolio-farhan <tugas.farhandzaky@gmail.com>
2019-12-09 19:29:46 +0700

    master page admin crated

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+
+@section('content')
+    <h1>It's Working</h1>
+@endsection
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+
+@section('content')
+    <h1>This is index post</h1>
+@endsection
